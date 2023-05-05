47884c22cb1759523d5967bbd96e13aa63b2b327
Radek <plocharadoslaw@gmail.com>
2023-05-05 09:12:13 +0200

    addded api for posts

--- server.js
+++ server.js
@@ -21,9 +21,51 @@ connection.connect((error) => {
   if (error) throw error;
   console.log('Successfully connected to the database.');
 });
-app.get('/users', function (req, res) {
-  res.send('Hello World!');
+app.get('/api/posts/friends/:user_id', function (req, res) {
+  // get all posts from user friends
+  connection.query(
+    'SELECT * FROM Posts WHERE user_id IN (SELECT friend_id FROM Friends WHERE user_id = ?)',
+    [req.params.user_id],
+    (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
 });
+app.post('/api/posts/:user_id', function (req, res) {
+  // create new post
+  connection.query(
+    'INSERT INTO Posts (user_id, content) VALUES (?, ?)',
+    [req.params.user_id, req.body.content],
+    (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+app.get('/api/posts/:user_id/', function (req, res) {
+  // get all posts from user
+  connection.query(
+    'SELECT * FROM Posts WHERE user_id = ?',
+    [req.params.user_id],
+    (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+app.get('/api/post/:post_id', function (req, res) {
+  // get post by id
+  connection.query(
+    'SELECT * FROM Posts WHERE post_id = ?',
+    [req.params.post_id],
+    (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
