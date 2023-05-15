6e86f56d8487dcac1f3f835fa0c2b996ae118fba
Radek <plocharadoslaw@gmail.com>
2023-05-15 10:00:28 +0200

    added user searching enpoint

--- server.js
+++ server.js
@@ -212,6 +212,18 @@ app.get('/api/user/comments/:username', async function (req, res) {
   );
 });
 
+app.get('/api/search/:search', async function (req, res) {
+  // search for users by username return 10 results
+  connection.query(
+    'SELECT * FROM Users WHERE username LIKE ? LIMIT 10',
+    ['%' + req.params.search + '%'],
+    (error, results) => {
+      if (error) res.status(404).send({ message: 'Users not found' });
+      res.status(200).json({ data: results });
+    }
+  );
+});
+
 app.get('/api/comments/:post_id', async function (req, res) {
   // get comments from post
   connection.query(
